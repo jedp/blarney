11c9e54706a4aac50bd52adc33510af0832f89f8
Jed Parsons <jed@squareup.com>
2016-07-26 09:36:07 -0700

    Add decrypt function for rot13

--- rot13.py
+++ rot13.py
@@ -8,6 +8,10 @@ rot13 = string.maketrans(
 def encrypt(s):
     return string.translate(s, rot13)
 
+
+def decrypt(s):
+    return string.translate(s, rot13)
+
 # Run tests.
 if __name__ == '__main__':
     assert(encrypt("Attack at dawn!") == "Nggnpx ng qnja!")
