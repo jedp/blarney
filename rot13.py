ad531bcc49d2faaeb43a2b3dc187627a40ba865f
Jed Parsons <jed@squareup.com>
2016-07-26 09:35:19 -0700

    Add test for encryption

--- rot13.py
+++ rot13.py
@@ -7,3 +7,7 @@ rot13 = string.maketrans(
 
 def encrypt(s):
     return string.translate(s, rot13)
+
+# Run tests.
+if __name__ == '__main__':
+    assert(encrypt("Attack at dawn!") == "Nggnpx ng qnja!")
