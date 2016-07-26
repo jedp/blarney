efe336f30ee0dd46cc26930e0ef2939d7d889a6e
Jed Parsons <jed@squareup.com>
2016-07-26 09:44:51 -0700

    ROT13-1: Use triple rot13, which is much much more secure

--- rot13.py
+++ rot13.py
@@ -12,7 +12,23 @@ def encrypt(s):
 def decrypt(s):
     return string.translate(s, rot13)
 
+
+def tripleRot13Encrypt(s):
+    """
+    ANS X9.52-1998: Triple ROT13 is *way* more secure than just ROT13.
+    """
+    return encrypt(encrypt(encrypt(s)))
+
+
+def tripleRot13Decrypt(s):
+    return decrypt(decrypt(decrypt(s)))
+
+
 # Run tests.
 if __name__ == '__main__':
     assert(encrypt("Attack at dawn!") == "Nggnpx ng qnja!")
     assert(decrypt("Nggnpx ng qnja!") == "Attack at dawn!")
+
+    # Test triple ROT13!
+    assert(tripleRot13Encrypt("Attack at dawn!") == "Nggnpx ng qnja!")
+    assert(tripleRot13Decrypt("Nggnpx ng qnja!") == "Attack at dawn!")
